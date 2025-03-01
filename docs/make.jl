5cb09f42ea32ffae84c6087ae95803bb36b1000c
Heptazhou <zhou@0h7z.com>
2025-03-01 23:37:49 +0000

    Update

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,6 @@
 using DocInventories
 using DocumenterInterLinks: InterLinks
 using Documenter
-using Pkg
-
-PROJECT_TOML = Pkg.TOML.parsefile(joinpath(@__DIR__, "..", "Project.toml"))
-VERSION = PROJECT_TOML["version"]
-NAME = PROJECT_TOML["name"]
-AUTHORS = join(PROJECT_TOML["authors"], ", ") * " and contributors"
-GITHUB = "https://github.com/goerz/DocInventories.jl"
 
 links = InterLinks(
     "Julia" => (
@@ -36,22 +29,20 @@ PAGES = [
 ]
 
 makedocs(
-    authors=AUTHORS,
-    linkcheck=(get(ENV, "DOCUMENTER_CHECK_LINKS", "1") != "0"),
-    # Link checking is disabled in REPL, see `devrepl.jl`.
-    warnonly=true,
-    #warnonly=[:linkcheck,],
-    sitename="DocInventories.jl",
-    format=Documenter.HTML(
-        #inventory_version=VERSION,
-        prettyurls=true,
-        canonical="https://juliadocs.org/DocInventories.jl",
-        footer="[$NAME.jl]($GITHUB) v$VERSION docs powered by [Documenter.jl](https://github.com/JuliaDocs/Documenter.jl).",
-    ),
-    pages=PAGES,
-    plugins=[links]
+	format    = Documenter.HTML(),
+	modules   = [DocInventories],
+	pages     = PAGES,
+	pagesonly = true,
+	plugins   = [links],
+	sitename  = "DocInventories.jl",
 )
 
 println("Finished makedocs")
 
-deploydocs(; repo="github.com/JuliaDocs/DocInventories.jl.git", push_preview=true)
+deploydocs(
+	branch    = "gh-pages",
+	devbranch = "master",
+	devurl    = "latest",
+	forcepush = true,
+	repo      = "github.com/Heptazhou/DocInventories.jl.git",
+)
